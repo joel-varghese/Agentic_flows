ac0f5083dcae825ddba84a0e654f1d43805efe9a
joel-varghese <joelvar17@outlook.com>
2025-11-24 17:11:48 -0500

    Multi Modal implementation

--- indexer.py
+++ indexer.py
@@ -1,11 +1,14 @@
 from typing import Annotated
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph,START,END
+from langgraph.prebuilt import ToolNode
+from langgraph.prebuilt import tools_condition
 from langgraph.graph.message import add_messages 
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from langchain_community.llms import HuggingFacePipeline
 from dotenv import load_dotenv
 from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
+from langchain_tavily import TavilySearch
 import torch
 import os
 
@@ -15,6 +18,7 @@ load_dotenv()
 base_model = "meta-llama/Llama-3.2-1B-Instruct"
 local_dir = "../llama3_local"
 api = os.getenv("HF_TOKEN")
+tavily = os.getenv("TAVILY_API")
 
 tokenizer = AutoTokenizer.from_pretrained(
     base_model,
@@ -65,23 +69,6 @@ def chatbot(state:State):
     return {"messages":[AIMessage(content=response)]}
 
 
-
-# Adding Node
-graph_builder.add_node("my_chat",chatbot)
-# Adding Edges
-graph_builder.add_edge(START,"my_chat")
-graph_builder.add_edge("my_chat",END)
-
-graph=graph_builder.compile()
-
-
-def answer_query(query):
-    # response = graph.invoke({"messages": [HumanMessage(content=query)]})
-    for event in graph.stream({"messages":[HumanMessage(content=query)]}):
-        print(event)
-
-    return "Done"
-
 def multiply(a:int,b:int)->int:
     """
         Multiple a and b
@@ -96,4 +83,71 @@ def multiply(a:int,b:int)->int:
     return a*b
 
 
+tool = TavilySearch(max_results=2)
+# tool.invoke("What is langgraph ?")
 
+
+
+tools=[tool,multiply]
+llm_with_tool = llm.bind_tools(tools)
+
+
+def tool_calling_llm(state:State):
+    messages = state["messages"]
+    if isinstance(messages[-1], HumanMessage):
+        prompt = messages[-1].content
+    elif isinstance(messages, str):
+        prompt = messages
+    else:
+        raise ValueError(f"Unsupported message format: {type(messages)}")
+    response = llm.invoke(prompt)
+
+    return {"messages":[AIMessage(content=response)]}
+
+# Adding Node
+graph_builder.add_node("tool_calling_llm", tool_calling_llm)
+graph_builder.add_node("tools",ToolNode(tools))
+# Adding Edges
+graph_builder.add_edge(START,"tool_calling_llm")
+graph_builder.add_conditional_edges(
+    "tool_calling_llm", tools_condition
+)
+graph_builder.add_edge("tools",END)
+
+
+graph=graph_builder.compile()
+
+def answer_query(query):
+    response = graph.invoke({"messages": [HumanMessage(content=query)]})
+    print(response)
+    print("_-----")
+    print(response["messages"][-1])
+    # for event in graph.stream({"messages":[HumanMessage(content=query)]}):
+    #     print(event)
+
+    return response["messages"][-1].content
+
+
+
+# Start
+# LLM + promt -> Chatbot 
+# 
+# 
+# 
+# External Yools  Tool Node
+# Make a tool Call || Tavily || -><- 
+# 
+# 
+# End 
+# How does chatbot know the when to use tools ?
+# LLM binds with the tools : 
+# Addition function added as tool to the LLM 
+# Doc String is used to know what are thre inputs and arguments : If they match LLM make a call to Tool
+# Instead of relying on its own response.
+# 
+# Binding tools with LLMs #
+
+
+
+
+# Stategraph
